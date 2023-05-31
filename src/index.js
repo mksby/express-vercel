c8ffa52b2634e174570b6ece7cfd189c2e4865d4
Maksim Troitski <maxim.troitsky@gmail.com>
2023-05-31 23:37:09 +0300

    comment oud product endpoint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const product = require("../api/product");
+// const product = require("../api/product");
 
-app.use(express.json({ extended: false }));
+// app.use(express.json({ extended: false }));
 
-app.use("/api/product", product);
+// app.use("/api/product", product);
 
 app.use('/api/welcome', (req, res) => {
     res.send({ message: 'Welcome to api!!' });
@@ -13,4 +13,4 @@ app.use('/api/welcome', (req, res) => {
 // const PORT = process.env.PORT || 8080;
 // app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
 
-// module.exports = app;
+module.exports = app;
